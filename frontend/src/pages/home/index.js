53eec9f639e26e125e163b134c5828ce2d728e4f
karthik_ironman <karthiksj008@gmail.com>
2022-12-11 08:50:35 +0530

    page traversing

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,16 +1,21 @@
 import "./home.scss";
 import MainBtn from "../../components/mainBtn";
+import { useNavigate } from "react-router-dom";
 const Home = () => {
+    const navigate = useNavigate();
+    const goTo = (page) => {
+        navigate(page)
+    }
     return (
         <div className="home-wrapper">
             <div className="welcome-card">
                 <p className="name"> Hi, Aravind </p>
                 <p className="greetings">Have  great day!</p>
             </div>
-            <MainBtn label="Random Vocabulary"/>
-            <MainBtn label="Grammar Builder"/>
-            <MainBtn label="Dialogues"/>
-            <MainBtn label="Free Flow Conversations"/>
+            <MainBtn label="Random Vocabulary" onClick={()=>goTo('random_vocabulary')}/>
+            <MainBtn label="Grammar Builder" onClick={()=>goTo('grammer_builder')}/>
+            <MainBtn label="Dialogues" onClick={()=>goTo('dialogues')}/>
+            <MainBtn label="Free Flow Conversations" onClick={()=>goTo('free_flow_conversations')}/>
         </div>
     )
 }
