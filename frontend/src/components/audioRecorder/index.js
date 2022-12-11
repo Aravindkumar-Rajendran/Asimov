f34b82e4658286d833495f1ccded83954d9b7c27
karthik_ironman <karthiksj008@gmail.com>
2022-12-11 18:03:00 +0530

    audio recorder button added

--- frontend/src/components/audioRecorder/index.js
+++ frontend/src/components/audioRecorder/index.js
@@ -1,5 +1,6 @@
 import "./audioRecorder.scss";
 import { useState } from "react";
+import Wave from 'react-wavify'
 
 const AudioRecorder = () => {
     const [recording, setRecording] = useState(false);
@@ -8,22 +9,40 @@ const AudioRecorder = () => {
         if (recording) {
             styleObj.height = '1px';
             styleObj.width = '100%';
-            styleObj.background = 'red';
+            styleObj.background = 'black';
             styleObj.border = 'none';
+            styleObj.top='3rem';
         }
         return styleObj;
     }
     const triggerRecord = () => {
         setRecording(true);
-        setTimeout(()=>{setRecording(false);},3000)
+        setTimeout(() => { setRecording(false); }, 3000)
     }
     return (
         <div className="audio-recorder-wrapper">
+            {recording && 
+                <div className="wave">
+                <Wave
+                fill='#76fede'
+                    paused={false}
+                    options={{
+                        height: 10,
+                        amplitude: 20,
+                        speed: 1,
+                        points: 5,
+                        active:false
+                    }}
+                >
+                    
+                </Wave>
+            </div>}
+
             <div className="recorder-btn" style={getRecordingBtnStyle()} onClick={triggerRecord}>
-             
+
             </div>
-            <input/>
-            <button>submit</button>
+            {/* <input />
+            <button>submit</button> */}
 
 
         </div>
