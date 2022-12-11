60e4b6a0e61edd28e7cdd0bed4f4168befe243a0
karthik_ironman <karthiksj008@gmail.com>
2022-12-11 16:53:08 +0530

    chat success and failure logic

---
+++ frontend/src/components/chatView/data.js
@@ -0,0 +1,40 @@
+export default [
+    {
+        type: 1,
+        text: "Hi, How are you",
+        original: "Hi, How are you",
+        flash:''
+    },
+    {
+        type: 2,
+        text: "Hi, How are you",
+        original: "Hi, How are you",
+        flash:''
+    },
+    {
+        type: 2,
+        text: "I'm doing well, thank you, How are you __________?",
+        original: "I'm doing well, thank you, How are you __________?",
+        state: 'correct',// correct, wrong,
+        flash:''
+    },
+    {
+        type: 1,
+        text: "Hi, How are you",
+        original: "Hi, How are you",
+        flash:''
+    },
+    {
+        type: 2,
+        text: "Hi, How are you",
+        original: "Hi, How are you",
+        flash:''
+    },
+    {
+        type: 2,
+        text: "I'm doing well, thank you, How are you __________? hank you, How are you __________",
+        original: "I'm doing well, thank you, How are you __________? hank you, How are you __________",
+        state: 'correct',// correct, wrong,
+        flash:''
+    }
+]
--- frontend/src/components/chatView/index.js
+++ frontend/src/components/chatView/index.js
@@ -1,119 +1,137 @@
 import { useState, useRef, useEffect } from "react"
-import "./chatView.scss"
-// import Shake from "reshake/dist/ShakeCrazy"
-import { ShakeCrazy as Shake } from "reshake"
+import "./chatView.scss";
+import { ShakeCrazy as Shake } from "reshake";
+import chat_data_initial from "./data"
 const ChatView = () => {
-    const chat_data_initial = [
-        {
-            type: 1,
-            text: "Hi, How are you",
-            original: "Hi, How are you"
-        },
-        {
-            type: 2,
-            text: "Hi, How are you",
-            original: "Hi, How are you"
-        },
-        {
-            type: 2,
-            text: "I'm doing well, thank you, How are you __________?",
-            original: "I'm doing well, thank you, How are you __________?",
-            state: 'correct'// correct, wrong
-        },
-        {
-            type: 1,
-            text: "Hi, How are you",
-            original: "Hi, How are you"
-        },
-        {
-            type: 2,
-            text: "Hi, How are you",
-            original: "Hi, How are you"
-        },
-        {
-            type: 2,
-            text: "I'm doing well, thank you, How are you __________? hank you, How are you __________",
-            original: "I'm doing well, thank you, How are you __________? hank you, How are you __________",
-            state: 'correct'// correct, wrong
-        }
-    ]
+    
     const [chat_data, setChat_data] = useState(chat_data_initial);
-    const [inputTxt, setInputTxt] = useState("");
-    const [refresh, setRefresh] = useState(true)
-    const [remove,setRemove] = useState(false)
-    const replaceBlank = () => {
-        console.log(inputTxt, chat_data)
-        let dta = chat_data;
-        let text_to_change = dta[dta.length - 1].original;
-        let changed = text_to_change.replace(/__________/g, '<span class="usinput"> '+ inputTxt+' </span>');
-        updateLastElem('correct', changed)
+    const [inputTxt, setInputTxt] = useState(""); //user input field/ this will be replaced by sppech to text
+    const [refresh, setRefresh] = useState(true); //used to reload the dom
+    const [remove, setRemove] = useState(false); //used to remove last element from dom
+    const [flash,setFlash] = useState(""); //to flash statuses such as correct / wrong and to perform remove operation
+    const bottomRef = useRef(null);
 
-
-    }
-    const updateLastElem = async(state = 'correct', text, remove = false) => {
-        let dta = chat_data;
-
-        dta[dta.length - 1] = {
-            ...dta[dta.length - 1],
-            original: dta[dta.length - 1].original,
-            text: text ? text : dta[dta.length - 1].text,
-            state: state ? state : dta[dta.length - 1].state
-        }
-        setChat_data(dta)
-        setRefresh(!refresh);
-        
-        if (remove) {
-            setRemove(true)
-        }
-    }
     const scroll = () => {
         // 👇️ scroll to bottom every time messages change
         console.log('scrolling')
         bottomRef.current?.scrollIntoView();
     }
-    const bottomRef = useRef(null);
+   
     useEffect(() => {
         // 👇️ scroll to bottom every time messages change
         scroll()
     }, [refresh]);
     useEffect(() => {
         const timer = window.setTimeout(() => {
-            if(remove == true){
-                let dta = chat_data;
+            let dta = chat_data;
+            if (remove == true) {
+               
                 dta.pop();
                 setChat_data(dta);
-                setRemove(false)
+                setRemove(false);
+
             }
-           
+            if (['loading','success','failure'].includes(flash)){
+                console.log({flash})
+                if(flash !== 'loading')//loading flashes will be infinite
+                dta[dta.length-1].flash = '';
+                setChat_data(dta);
+                if(flash !== 'loading')//loading flashes will be infinite
+                setFlash("")
+            }
+            //resetting all the status
+            if(refresh != '123'){
+                
+           dta[dta.length-1].state = 'correct';
+           setChat_data(dta);
+           setRefresh('123')
+            }
+
+
         }, 1000);
         return () => {
-          window.clearTimeout(timer);
+            window.clearTimeout(timer);
         };
-      }, [remove,chat_data]);
-      const insertElement  =() => {
+    }, [remove,  chat_data, flash,refresh]);
+    const replaceBlank = (clear=false) => {
+        let dta = chat_data;
+        if(clear){
+            let text_to_change = dta[dta.length - 1].original;
+            updateLastElem('correct', text_to_change)
+            setInputTxt("")
+        }else{
+            let text_to_change = dta[dta.length - 1].original;
+            let changed = text_to_change.replace(/__________/g, '<span class="usinput"> ' + inputTxt + ' </span>');
+            updateLastElem('correct', changed)
+            setInputTxt("")
+        }
+       
+    }
+    const updateLastElem = async (state, text, remove = false,flash) => {
+        let dta = chat_data;
+        dta[dta.length - 1] = {
+            ...dta[dta.length - 1],
+            original: dta[dta.length - 1].original,
+            text: text ? text : dta[dta.length - 1].text,
+            state: state ? state : dta[dta.length - 1].state,
+            flash:flash!= undefined?flash:dta[dta.length -1].flash
+        }
+        setChat_data(dta)
+       
+        if(flash != undefined){
+            setFlash(flash);
+        }
+        setRemove(remove)
+        setRefresh(!refresh);
+    }
+    const insertElement = () => {
         let randomIndex = Math.round(Math.random() * (chat_data_initial.length - 1))
-        setChat_data([...chat_data,chat_data_initial[randomIndex]]);setRefresh(!refresh)
-      }
+        setChat_data([...chat_data, chat_data_initial[randomIndex]]); setRefresh(!refresh)
+    }
+    const insertEntry = (type = 2) => {
+        let newElement = {
+            type,
+            original:inputTxt,
+            text:inputTxt,
+            state:'correct',
+            flash:''
+        }
+        setChat_data([...chat_data,newElement]);
+        setInputTxt("");
+        setRefresh(!refresh)
+    }
     return (
         <>
             <div className="chat-view-wrapper" id="random_chat_parent" key={refresh} >
                 {chat_data.map((x, index) => {
-                    return <Box key={index} data={x} id={"random_chat_" + index} />
+                    return <Box key={index+refresh} data={x} id={"random_chat_" + index} />
                 })}
                 <div ref={bottomRef} />
             </div>
             <div className="controls">
-            <input placeholder="replace blank" value={inputTxt} onChange={(e) => {  setInputTxt(e.target.value) }} />
-            <div onClick={()=>replaceBlank()}> Replace text</div>
-            <div onClick={()=>updateLastElem('wrong',undefined,true)}>Make Last component wrong and remove</div>
-            <div onClick={insertElement}>insert</div>
+                <div style={{ display: 'flex',flexDirection:'column' }}>
+                    <input placeholder="replace blank / answer" value={inputTxt} onChange={(e) => { setInputTxt(e.target.value) }} />
+                    <div onClick={() => replaceBlank()}> Replace text</div>
+                    <div onClick={() => replaceBlank(true)}> Reset text</div>
+                    <div onClick={() => insertEntry()}>ANSWER</div>
+                    <div onClick={() => insertEntry(1)}>INSERT QUESTION</div>
+                </div>
+                
+                <div onClick={() => updateLastElem('wrong', undefined, true)}>Make Last component wrong and remove</div>
+                <div onClick={() => updateLastElem('wrong', undefined)}>Make Last component wrong and dont remove</div>
+                <div onClick={insertElement}>insert Random Element</div>
+                <div onClick={()=>updateLastElem(undefined,undefined,undefined,'loading')}>SET lOADING</div>
+                <div onClick={()=>updateLastElem(undefined,undefined,false,'success')}>SET SUCCESS</div>
+                <div onClick={()=>updateLastElem(undefined,undefined,undefined,'failure')}>SET FAILURE </div>
+                <div onClick={()=>updateLastElem(undefined,undefined,undefined,'')}>RESSET loading</div>
+                <div onClick={()=>setRefresh(!refresh)}>SCROLL TO LAST</div>
             </div>
-            
+
         </>
 
     )
 }
-const Box = ({ data, id }) => {
+const Box = ({ data, id,refresh }) => {
     const getStyle = (type, state) => {
         let styleObj = {};
         styleObj.alignSelf = (type === 1) ? 'start' : 'end';
@@ -122,7 +140,7 @@ const Box = ({ data, id }) => {
         } else {
             styleObj.background = 'linear-gradient(246.39deg, #00FFF0 1.35%, #02FA92 102.51%)'
         }
-        if(state == 'wrong'){
+        if (state == 'wrong') {
             styleObj.background = 'red'
         }
         return styleObj
@@ -138,12 +156,31 @@ const Box = ({ data, id }) => {
         }
         return styleObj;
     }
+    const FetchFlashMessage = () => {
+          let img_src = "";
+          let randomNumber = Math.round(Math.random()*5)
+          if(data.flash == 'success'){
+              img_src = 'success_'+4+".gif"
+          }else if(data.flash == 'failure'){
+            img_src = 'failure_'+3+".gif"
+          }else if(data.flash == 'loading'){
+            img_src = 'loading_'+0+".gif"
+          }
+         return <img className="flash_img" src={img_src}></img>
+    }
     return (
 
-        <div className="chat-box-units" style={getStyle(data.type, data.state)} id={id}>
+        <div className="chat-box-units" style={getStyle(data.type, data.state)} id={id} key={refresh}>
             <div className="chat-box-circle" style={ciclePosition(data.type)}>
 
             </div>
+            {
+                ['loading','success','failure'].includes(data.flash)  && 
+                <div className="flashMessage" style={getStyle(data.type,data.state)}>
+                    <FetchFlashMessage/>
+               </div>
+            }
+            
             <Shake
                 h={5}
                 v={5}
@@ -154,9 +191,10 @@ const Box = ({ data, id }) => {
                 fixed={true}
                 fixedStop={false}
                 freez={false}
-                active={data.state == 'wrong'}>
+                active={data.state === 'wrong'}>
                 <div dangerouslySetInnerHTML={{ __html: data.text }}></div>
             </Shake>
+         
         </div>
     )
 }
